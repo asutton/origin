29b77a3ae561162f9320a475e6957912689b8da1
Andrew Sutton <andrew.n.sutton@gmail.com>
2013-10-17 13:19:49 -0400

    Clean up previous test file.

--- origin/range/algorithm.test/all_of.cpp
+++ origin/range/algorithm.test/all_of.cpp
@@ -6,30 +6,14 @@
 
 #include <cassert>
 #include <vector>
-#include <iostream>
 
-#include <origin/core/type.hpp>
 #include <origin/range/algorithm.hpp>
-
-template<origin::Input_range R, typename P>
-    inline bool 
-    foo(R&& range, P pred) {
-      using namespace origin;
-
-      using I = Iterator_type<R>;
-      std::cout << typestr<I>() << '\n';
-      std::cout << typestr<Value_type<I>>() << '\n';
-      std::cout << Range_query<R, P>() << '\n';
-
-      return true;
-    }
 
 bool pos(int n) { return n > 0; }
 
 int main() 
 {
   int a[3] { 1, 2, 3 };
-  // assert(foo(a, pos));
   assert(origin::all_of(a, pos));
 
   std::vector<int> v { 1, 2, 3 };
