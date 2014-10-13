f2c8e09e5e64b5373602cbbf44c4a3553112fbb9
Andrew Sutton <andrew.n.sutton@gmail.com>
2014-10-13 16:51:08 -0400

    Building out actual libraries.

---
+++ origin/core/concepts.cpp
@@ -0,0 +1,5 @@
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+#include "concepts.hpp"
