53a9167bc82ca451b28129cca2040369f95a48ca
Andrew Sutton <andrew.n.sutton@gmail.com>
2015-09-07 12:09:06 -0400

    Adding examples.

---
+++ examples/hello/hello.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2009-2015 Andrew Sutton
+// All rights reserved
+
+#include <origin/generic.hpp>
+
+#include <iostream>
+
+template<origin::Output_streamable T>
+void f(T const& t)
+{
+  std::cout << t << '\n';
+}
+
+
+int
+main()
+{
+  f("Hello, world!");
+}
